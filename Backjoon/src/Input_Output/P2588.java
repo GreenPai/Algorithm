15665012cd4f337a68d35d637b3958d459b82475
roberty <clsrntkdgh@gmail.com>
2025-04-07 08:30:31 +0900

    입출력과 사칙연산

--- Backjoon/src/Input_Output/P2588.java
+++ Backjoon/src/Input_Output/P2588.java
@@ -12,10 +12,15 @@ public class P2588 {
         int a = sc.nextInt();
         int sum = 0;
 
-        String b = sc.nextLine();
-        char[] c = b.toCharArray();
+        int b = sc.nextInt();
+        int d1 = b%10;
+        int d2 = (b/10)%10;
+        int d3 = b/100;
 
-
+        System.out.println(a*d1);
+        System.out.println(a*d2);
+        System.out.println(a*d3);
+        System.out.println(a*b);
 
 
     }
